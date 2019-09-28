2f788f33a52468cce27e3a9b567c88b81e9c2914
Anatoly <kypiwindy1@gmail.com>
2019-09-28 23:49:32 +0300

    clean

--- AnekdotBot/Services/UpdateService.cs
+++ AnekdotBot/Services/UpdateService.cs
@@ -45,33 +45,6 @@ namespace AnekdotBot.Services
 
             await _botService.Client.SendTextMessageAsync(update.Message.Chat.Id, joke);
 
-
-            //update.Type==
-
-            //var message = update.Message;
-
-            //_logger.LogInformation("Received Message from {0}", message.Chat.Id);
-
-            //if (message.Type == MessageType.Text)
-            //{
-            //    // Echo each Message
-            //    await _botService.Client.SendTextMessageAsync(message.Chat.Id, message.Text);
-            //}
-            //else if (message.Type == MessageType.Photo)
-            //{
-            //    // Download Photo
-            //    var fileId = message.Photo.LastOrDefault()?.FileId;
-            //    var file = await _botService.Client.GetFileAsync(fileId);
-
-            //    var filename = file.FileId + "." + file.FilePath.Split('.').Last();
-
-            //    using (var saveImageStream = System.IO.File.Open(filename, FileMode.Create))
-            //    {
-            //        await _botService.Client.DownloadFileAsync(file.FilePath, saveImageStream);
-            //    }
-
-            //    await _botService.Client.SendTextMessageAsync(message.Chat.Id, "Thx for the Pics");
-            //       }
         }
     }
 }
